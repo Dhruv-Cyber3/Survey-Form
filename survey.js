1f90d335f8b83c249e8b93a995fddf3a344eb9e9
Dhruv Choudhary <totallynoobcoder@gmail.com>
2021-02-13 23:09:29 +0530

    removed survey.js

--- survey.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('#form-box');
-const submit = document.querySelector('#submit');
-const thank = document.querySelector('#thank-you');
-
-submit.addEventListener('click', function (e) {
-    form.style.display = 'none';
-    thank.style.display = 'block';
-})
